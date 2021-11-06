5c799700501c3279ea680f8f6aac259563fbfe80
soushi <sosimds@gmail.com>
2021-11-06 16:09:44 +0900

    markovSearch.pyの機能追加。(キーが配列形式でも検索できるように、ゆるい条件で検索する機能を追加。)

--- markovSearch.py
+++ markovSearch.py
@@ -3,18 +3,27 @@ import json
 
 DBPATH = 'markov.sqlite3'
 
-def main(dbpath, keyword):
-    db = sqlite3.connect(dbpath)
-    print('='*30)
-    keyword = '%' + keyword + '%'
+class MarkovSearcher:
+    def __init__(self, dbpath):
+        self.db = sqlite3.connect(dbpath)
 
-    res = db.execute('SELECT key, value FROM items WHERE key LIKE ?', (keyword,))
+    def close(self):
+        self.db.close()
 
-    for key, value in res:
-        values = json.loads(value)
-        values = ','.join(values.keys())
-        print(key+' : '+values)
-    print('='*30)
+    def search(self, keyword, loose=False):
+        db = self.db
+        print('='*30)
+        if loose:
+            keyword = '%'.join(keyword)
+        keyword = '%' + keyword + '%'
+
+        res = db.execute('SELECT key, value FROM items WHERE key LIKE ?', (keyword,))
+
+        for key, value in res:
+            values = json.loads(value)
+            values = ','.join(values.keys())
+            print(key+' : '+values)
+        print('='*30)
 
 if __name__ == '__main__':
     import argparse
@@ -22,11 +31,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('filepath', default=DBPATH, type=str, help='データベースのパス')
     parser.add_argument('--keyword', '-k', type=str, help='検索キーワード')
+    parser.add_argument('--loose', '-l', action='store_true', help='検索条件を緩くする')
 
     args = parser.parse_args()
 
+    searcher = MarkovSearcher(args.filepath)
+
     if args.keyword:
-        main(args.filepath, args.keyword)
+        searcher.search(args.keyword, args.loose)
     else:
         while 1:
-            main(args.filepath, input('キーワード: '))
+            keyword = input('キーワード: ')
+            if keyword == '/quit':
+                break
+            searcher.search(keyword, args.loose)
+    sercher.close()
