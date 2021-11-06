d4a3e36966a6c88387b617bf598771963fe47823
soushi <sosimds@gmail.com>
2021-11-06 12:12:31 +0900

    ログレベルを調整。形態素解析中に進捗度を表示する機能を追加。

--- jpTextAnalyzer.py
+++ jpTextAnalyzer.py
@@ -6,38 +6,40 @@ from janome.tokenizer import Tokenizer
 
 
 class Analyzer:
-    def __init__(self, text=None):
+    def __init__(self):
         self._logger = logging.getLogger(f'{__name__}.{self.__class__.__name__}')
         self.nodes = []
         self.DBPATH = 'markov.sqlite3'
-
-        if text:
-            self.analyze(text)
 
     def _getDb(self):
         self._logger.debug('connecting database "{}"'.format(self.DBPATH))
         return sqlite3.connect(self.DBPATH)
 
-    def analyze(self, text):
-        self._logger.debug('analyzing')
+    def analyze(self, text, show_progress=False, progress_freq=100):
+        self._logger.info('analyzing')
 
         text = re.sub('[^\S\n]+', '', text)
         lines = re.split('\n+', text)
 
         nodes = []
         tokenizer = Tokenizer()
-        self._logger.debug('tokenizing')
-        for line in lines:
-            nodes.extend(tokenizer.tokenize(line))
-        self._logger.debug('tokenizing is completed. result : {} words'.format(len(nodes)))
+        self._logger.info('tokenizing : {} lines'.format(len(lines)))
+        for i in range(len(lines)):
+            nodes.extend(tokenizer.tokenize(lines[i]))
+            if show_progress:
+                if (i % progress_freq) == 0:
+                    print('\rtokenize : {:>7.3f} %   {} / {}'.format((i/len(lines))*100, i, len(lines)), end='')
+        if show_progress:
+            print()
+        self._logger.info('tokenizing is completed. result : {} words'.format(len(nodes)))
 
         self.nodes.extend(nodes)
-        self._logger.debug('add to node. result : {} words'.format(len(self.nodes)))
+        self._logger.info('add to node. result : {} words'.format(len(self.nodes)))
 
-        self._logger.debug('analyzing is completed')
+        self._logger.info('analyzing is completed')
 
     def countWord(self, num=1, key_tuple=False):
-        self._logger.debug('counting words')
+        self._logger.info('counting words')
         result = {}
         for i in range(num-1, len(self.nodes)):
             currentNodes = self.nodes[i-num+1:i+1]
@@ -49,14 +51,14 @@ class Analyzer:
             result[key] += 1
         result = sorted(result.items(), key=lambda i:i[1], reverse=True)
 
-        self._logger.debug('counting is completed')
+        self._logger.info('counting is completed')
         return result
 
     def makeMarkov(self, wordNum=1, dic={}, key_tuple=True, value_simple=True):
         if wordNum <= 0:
             self._logger.error('wordNum must be greater than 1')
             raise Exception('wordNum must be greater than 1')
-        self._logger.debug('making markov : option (wordNum:{}, dict input:{}, key_tuple:{} value\simple:{}'.format(wordNum, dic!={}, key_tuple, value_simple))
+        self._logger.info('making markov : option (wordNum:{}, dict input:{}, key_tuple:{} value\simple:{}'.format(wordNum, dic!={}, key_tuple, value_simple))
         num = wordNum + 1
         result = dic
         for i in range(num+1, len(self.nodes)):
@@ -75,20 +77,20 @@ class Analyzer:
                 if value not in result[key]:
                     result[key][value] = 0
                 result[key][value] += 1
-        self._logger.debug('making markov is completed')
+        self._logger.info('making markov is completed')
         return result
 
     def saveMarkov_sqlite(self, wordNum=1, key_tuple=True):
-        self._logger.debug('saving markov to database')
+        self._logger.info('saving markov to database')
         markov = self.makeMarkov(wordNum, key_tuple=key_tuple, value_simple=False)
         
         self.mergeMarkovToDb(markov, key_tuple)
 
-        self._logger.debug('saving is complete')
+        self._logger.info('saving is complete')
         return
 
     def mergeMarkovToDb(self, markov, key_tuple):
-        self._logger.debug('merge to db')
+        self._logger.info('merge to db')
         self.checkDb()
 
         db = None
@@ -110,7 +112,7 @@ class Analyzer:
                     out_value[in_value_key] += in_value[in_value_key]
 
                 db.execute('UPDATE items SET value = ? WHERE key = ?', (json.dumps(out_value, ensure_ascii=False), in_key))
-            self._logger.debug('committing to database')
+            self._logger.info('committing to database')
             db.commit()
         except:
             self._logger.error('database writing error')
@@ -119,10 +121,10 @@ class Analyzer:
             if db:
                 db.close()
                 self._logger.debug('database is closed')
-        self._logger.debug('merge is complete')
+        self._logger.info('merge is complete')
 
     def loadMarkovFromDb(self, dbpath, key_tuple=True):
-        self._logger.debug('load markov from db')
+        self._logger.info('loading markov from db')
         db = None
         try:
             db = sqlite3.connect(dbpath)
@@ -140,19 +142,19 @@ class Analyzer:
             if key_tuple:
                 key = tuple(json.loads(key))
             markov[key] = json.loads(value)
-        self._logger.debug('loading is complete')
+        self._logger.info('loading is complete')
 
         return markov
 
     def mergeDbToDb(self, in_dbpath, key_tuple=True):
-        self._logger.debug('merging DB to DB')
+        self._logger.info('merging DB to DB')
         markov = self.loadMarkovFromDb(in_dbpath, key_tuple)
         self.mergeMarkovToDb(markov, key_tuple)
-        self._logger.debug('merging DB to DB is complete')
+        self._logger.info('merging DB to DB is complete')
 
 
     def initDb(self):
-        self._logger.debug('initializing database')
+        self._logger.info('initializing database')
         db = None
         try:
             db = self._getDb()
@@ -164,9 +166,10 @@ class Analyzer:
             if db:
                 db.close()
                 self._logger.debug('database is closed')
+        self._logger.info('initializing is complete')
 
     def checkDb(self):
-        self._logger.debug('checking database')
+        self._logger.info('checking database')
         try:
             db = self._getDb()
             db.execute('select * FROM items LIMIT 1').fetchone()
@@ -180,7 +183,7 @@ class Analyzer:
                 self._logger.debug('database is closed')
         if not initialized:
             self.initDb()
-        self._logger.debug('cheking database is completed')
+        self._logger.info('cheking database is completed')
 
         
             
@@ -191,48 +194,20 @@ def main():
     parser.add_argument('command', type=str, choices=['markov', 'count', 'merge'], help='コマンドを入力')
     parser.add_argument('--input', '-i', type=str, help='読み込むファイルパス')
     parser.add_argument('--out', '-o', type=str, help='出力ファイルパス。markovの場合はsqlite3、countの場合はテキストファイル')
+    parser.add_argument('--debug', '-d', default='info', choices=['debug', 'info'], type=str, help='表示するログレベル')
     parser.add_argument('--enc', '-e', default='utf-8', type=str, help='読み込むファイルのエンコード')
     parser.add_argument('--word_num', '-n', default=3, type=int, help='解析するときにキーとする単語数')
     parser.add_argument('--key_array', '-k', action='store_false', help='キーを配列形式にしない')
     parser.add_argument('--sep', '-s', default=':', type=str, help='出力時にkeyとvalueの間に入れるセパレーター')
+    parser.add_argument('--show_progress', '-sp', action='store_true', help='形態素解析中に進捗度を表示する')
+    parser.add_argument('--progress_freq', '-pf', default=100, type=int, help='進捗度を表示する頻度')
 
     args = parser.parse_args()
 
-    if args.command == 'merge':
-        analyzer = Analyzer()
-        analyzer.DBPATH = args.out
-        analyzer.mergeDbToDb(args.input)
-
-    if args.command in ['count', 'markov']:
-        analyzer = Analyzer(open(args.input, encoding=args.enc).read())
-
-        if args.command == 'count':
-            logger.debug('単語のカウントを行います')
-
-            count = analyzer.countWord(args.word_num)
-            texts = ''
-            for key, value in count:
-                texts += '{}{}{}'.format(key, args.sep, value) + '\n'
-            if args.out:
-                logger.debug('ファイル書き出し中')
-                f = open(args.out, 'w', encoding='utf-8')
-                f.write(texts)
-                f.close()
-                logger.debug('完了')
-            else:
-                print(texts)
-        elif args.command == 'markov':
-            logger.debug('マルコフ連鎖の作成。sqliteでの出力を行います')
-            if args.out:
-                analyzer.DBPATH = args.out
-            analyzer.saveMarkov_sqlite(args.word_num, key_tuple=args.key_array)
-            logger.debug('sqlite出力完了')
-    logger.debug('すべての操作が完了。')
-            
-
-
-if __name__ == '__main__':
-    LOGLEVEL = logging.DEBUG
+    if args.debug == 'debug':
+        LOGLEVEL = logging.DEBUG
+    elif args.debug == 'info':
+        LOGLEVEL = logging.INFO
     logger = logging.getLogger(__name__)
     streamHandler = logging.StreamHandler()
     streamHandler.setLevel(LOGLEVEL)
@@ -240,4 +215,39 @@ if __name__ == '__main__':
     logger.addHandler(streamHandler)
     logger.setLevel(LOGLEVEL)
 
+    if args.command == 'merge':
+        analyzer = Analyzer()
+        analyzer.DBPATH = args.out
+        analyzer.mergeDbToDb(args.input)
+
+    if args.command in ['count', 'markov']:
+        analyzer = Analyzer()
+        analyzer.analyze(open(args.input, encoding=args.enc).read(), show_progress=args.show_progress, progress_freq=args.progress_freq)
+
+        if args.command == 'count':
+            logger.info('単語のカウントを行います')
+
+            count = analyzer.countWord(args.word_num)
+            texts = ''
+            for key, value in count:
+                texts += '{}{}{}'.format(key, args.sep, value) + '\n'
+            if args.out:
+                logger.info('ファイル書き出し中')
+                f = open(args.out, 'w', encoding='utf-8')
+                f.write(texts)
+                f.close()
+                logger.info('完了')
+            else:
+                print(texts)
+        elif args.command == 'markov':
+            logger.info('マルコフ連鎖の作成。sqliteでの出力を行います')
+            if args.out:
+                analyzer.DBPATH = args.out
+            analyzer.saveMarkov_sqlite(args.word_num, key_tuple=args.key_array)
+            logger.info('sqlite出力完了')
+    logger.info('すべての操作が完了。')
+            
+
+
+if __name__ == '__main__':
     main()
